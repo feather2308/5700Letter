29105cd85b72b9f80d0edc265f2a926137842da8
feather2308 <feather2308@naver.com>
2025-12-13 19:53:44 +0900

    controller

--- Springboot/src/main/java/letter5700/controller/web/RecordController.java
+++ Springboot/src/main/java/letter5700/controller/web/RecordController.java
@@ -1,7 +1,30 @@
 package letter5700.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class RecordController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/loading")
+    public String loading() {
+        return "loading";
+    }
+
+    @GetMapping("/result")
+    public String result() {
+        return "result";
+    }
+
+    @GetMapping("/result-detail")
+    public String resultDetail() {
+        return "result-detail";
+    }
 }
